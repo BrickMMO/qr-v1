d46cad86238fda04bc98c8e2a5c3b5869f8e4172
Adam <thomasadam83@hotmail.com>
2024-10-24 11:20:49 -0400

    Readded countdown

--- qrRedirect.js
+++ qrRedirect.js
@@ -4,7 +4,8 @@ new Vue({
     redirecting: true,
     error: false,
     errorMessage: "",
-    qrCode: [],
+    qrCodeUrl: "",
+    countDown: 5,
   },
   created() {
     let uniqueId = window.location.href.split("/").pop();
@@ -17,18 +18,28 @@ new Vue({
     }
   },
   methods: {
+    async incCountDown() {
+      setTimeout(() => {
+        if (this.countDown > 0) {
+          this.countDown--;
+          console.log(this.countDown);
+          this.incCountDown();
+        } else {
+          window.location.href = this.qrCodeUrl;
+        }
+      }, 1000);
+    },
     async loadRedirect(uniqueId) {
       try {
-        // change the API url
         const response = await fetch(
           `https://console.brickmmo.com/api/qr/scan/${uniqueId}`
         );
         const data = await response.json();
         if (!data.error) {
-          this.qrCode = data.qr;
-          setTimeout(() => {
-            window.location.href = this.qrCode.url;
-          }, 20000);
+          this.qrCodeUrl = data.qr.url;
+          this.incCountDown();
+          console.log(data.qr.url);
+          console.log(this.qrCodeUrl);
         } else {
           throw new Error(data.message);
         }
